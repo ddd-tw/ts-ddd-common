221adf7e0a2783d4dfd90330753fbe1a9a2c2a3b
“fong” <fong1143@gmail.com>
2020-10-20 23:24:05 +0800

    chores: remove files in build

--- build/src/domain/GenericRepository.d.ts
+++
@@ -1,9 +0,0 @@
-import { AggregateRoot } from './AggregateRoot';
-import { EntityId } from './EntityId';
-export interface GenericRepository<Id extends EntityId<unknown>, A extends AggregateRoot<Id, {}>> {
-    nextId(): Id;
-    exist(id: Id): Promise<boolean>;
-    getById(id: Id): Promise<A | undefined>;
-    save(entity: A): Promise<void>;
-    remove(id: Id): Promise<void>;
-}
